9dc8d4b3be39228530252a02ad419c8f7a65f2c8
Alex Fonte <aramosdf@gmail.com>
2024-10-30 23:18:07 +0100

    Fieat: ex3_c, falta luckynumbers

--- PEC2_Ej3/c_reduce/core.js
+++ PEC2_Ej3/c_reduce/core.js
@@ -1,13 +1,21 @@
 function sum(array) {
-  // your code here
+  return array.reduce((acc, value) => acc + value, 0);
 }
 
 function productAll(array) {
   // your code here
+  return array.reduce((acc, value) => 
+    acc * value.reduce((acc1, value1) => acc1 * value1, 1)
+  , 1);
 }
 
 function objectify(array) {
   // your code here
+  return array.reduce(
+    (object, value) => {
+      object[value[0]] = value[1];
+      return object;
+    },{});
 }
 
 function luckyNumbers(array) {
