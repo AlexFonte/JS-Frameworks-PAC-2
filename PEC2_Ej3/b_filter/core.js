ea07cc5479768f3328c8b26ee4e6be5d1ce31d9b
Alex Fonte <aramosdf@gmail.com>
2024-10-30 23:06:28 +0100

    Fix Ex3_b

--- PEC2_Ej3/b_filter/core.js
+++ PEC2_Ej3/b_filter/core.js
@@ -1,17 +1,29 @@
 function onlyEven(array) {
   // your code here
+  return array.filter(number => number % 2 === 0);
 }
 
 function onlyOneWord(array) {
   // your code here
+  return array.filter(word => !/\s/.test(word));
 }
 
 function positiveRowsOnly(array) {
   // your code here
+  return array.filter(row => row.filter(n => n < 0).length === 0);
 }
 
 function allSameVowels(array) {
   // your code here
+  const arr = array.filter(word => {
+    const vowels = word.match(/[aeiou]/gi);
+    // console.log(vowels);
+    const unique = new Set(vowels); //creem un nou Set, ja quen no repeteix valors
+    // console.log(unique)
+    return unique.size === 1; // si el size del Set es 1 llavors vol dir que totes les vocals son iguals. i l'afegim al array.
+  });
+  // console.log(arr);
+  return arr;
 }
 
 module.exports = {
