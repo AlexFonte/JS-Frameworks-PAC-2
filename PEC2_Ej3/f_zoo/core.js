a0c896bbb04952b4a4de026ea40c6cdc8fe1c0ac
Alex Fonte <aramosdf@gmail.com>
2024-11-02 23:31:58 +0100

    Fixing exercice 3;

--- PEC2_Ej3/f_zoo/core.js
+++ PEC2_Ej3/f_zoo/core.js
@@ -4,7 +4,7 @@ function entryCalculator(entrants) {
   // your code here
   if (!entrants || Object.entries(entrants).length === 0) return 0;
 
-  return Object.entries(entrants).reduce((total, person) => { 
+  return Object.entries(entrants).reduce((total, person) => {
     /* console.log(person); */
     return total + (prices[person[0]] * person[1])
   }, 0);
@@ -13,14 +13,30 @@ function entryCalculator(entrants) {
 
 function schedule(dayName) {
   // your code here
+  console.log(dayName);
+  const hores = !dayName ? hours : { [dayName]: hours[dayName] }
+  const result = Object.entries(hores).reduce((sentence, [day, { open, close }]) => {
+    sentence[day] = getHumanHoures(open, close);
+    console.log(sentence);
+    return sentence;
+  }, {});
+  console.log(result);
+  return result;
 }
 
 function animalCount(species) {
   // your code here
+  const animalCount = animals.reduce((acc, { name, residents }) => {
+    acc[name] = residents.length;
+    return acc;
+  }, {});
+  return species ? animalCount[species] : animalCount;
 }
 
 function animalMap(options) {
   // your code here
+  console.log(options);
+  
 }
 
 function animalPopularity(rating) {
@@ -49,6 +65,13 @@ function managersForEmployee(idOrName) {
 
 function employeeCoverage(idOrName) {
   // your code here
+}
+
+// ############## EXTRA FUNCTIONS #################
+function getHumanHoures(open, close) {
+  if (open === 0 && close === 0) return 'CLOSED';
+
+  return `Open from ${open}am until ${close - 12}pm`;
 }
 
 module.exports = {
